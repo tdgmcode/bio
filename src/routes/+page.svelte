ef60a1c7f4403eef1c71aab8d54ba84bd0829e30
tdgmcode <tdgmdev@gmail.com>
2023-04-04 06:20:35 -0400

    Added NeoScratchTree and Scratch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -76,9 +76,11 @@
     <h2>Projects</h2>
     <div class='hwrap'>
         <Project name='Sanifae Legacy' url='https://sanifae.tdgmdev.net/users/tdgmdev' img='https://sanifae.tdgmdev.net/icon_sanifae_black.svg' />
+        <Project name='NeoScratchTree' url='https://tree.tdgmdev.net/?id=817940396' img='/img/favicon.svg' />
     </div>
     <h2>Contacts</h2>
     <div class='hwrap'>
+        <Project name='Scratch' url='https://scratch.mit.edu/users/malloc62/' img='https://scratch.mit.edu/favicon.ico' />
         <Project name='GitHub' url='https://github.com/tdgmcode' img='/img/github-mark-white.svg' />
         <Project name='FANDOM' url='https://alldimensions.fandom.com/User:08dravennew' img='https://static.wikia.nocookie.net/alldimensions/images/4/4a/Site-favicon.ico' />
         <Project name='YouTube' url='https://www.youtube.com/channel/UCX9pYmio1T6yqS-ZjqyNnlg' img='https://www.youtube.com/favicon.ico' /> 
